773eef5bd1d94ae900645cb19ef740f1a629c396
gi0v1 <giovipegoraro@gmail.com>
2023-02-21 17:34:11 +0100

    Squares detection now working

--- code/Squares_Detection.py
+++ code/Squares_Detection.py
@@ -1,33 +1,57 @@
-# import the necessary packages
-import numpy as np
-import argparse
 import cv2
+import numpy as np
 
-cap = cv2.VideoCapture(r"C:\Users\giovi\Desktop\RoboCup\Sync2\newCode\vids\2023-02-20 ore 18.57.44_mod.mp4")
+def Detect_Squares(img):
+    # convert to hsv colorspace
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help = "path to the image")
-args = vars(ap.parse_args())
+    # lower bound and upper bound for Green color
+    lower_bound = np.array([50, 20, 20])	 
+    upper_bound = np.array([100, 255, 255])
 
-while True:
-	# load the image
-	ret, image = cap.read()
+    # find the colors within the boundaries
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
 
-	# define the list of boundaries
-	boundaries = [
-		([0, 100, 0], [255, 100, 255])
-	]
+    #define kernel size  
+    kernel = np.ones((7,7),np.uint8)
 
-	# loop over the boundaries
-	for (lower, upper) in boundaries:
-		# create NumPy arrays from the boundaries
-		lower = np.array(lower, dtype = "uint8")
-		upper = np.array(upper, dtype = "uint8")
-		# find the colors within the specified boundaries and apply
-		# the mask
-		mask = cv2.inRange(image, lower, upper)
-		output = cv2.bitwise_and(image, image, mask = mask)
-		# show the images
-		cv2.imshow("images", np.hstack([image, output]))
-		cv2.waitKey(1)
+    # Remove unnecessary noise from mask
+
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+    # Segment only the detected region
+    segmented_img = cv2.bitwise_and(img, img, mask=mask)
+
+    return segmented_img, mask
+
+
+
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(r"C:\Users\giovi\Desktop\RoboCup\Sync2\newCode\vids\2023-02-20 ore 18.57.44_mod.mp4")
+
+    while True:
+        ret, frame = cap.read()
+
+        img = cv2.resize(frame,(160, 120))
+
+        segmented_img, mask = Detect_Squares(img)
+
+        # Find contours from the mask
+
+        contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        output = cv2.drawContours(segmented_img, contours, -1, (0, 0, 255), 3)
+
+        # Showing the output
+
+        cv2.imshow("Output", output)
+
+        cv2.imshow("Squares", mask)
+        
+        if cv2.waitKey(1) & 0xff == ord('q'):   # 1 is the time in ms
+            break
+    cap.release()
+    cv2.destroyAllWindows()
